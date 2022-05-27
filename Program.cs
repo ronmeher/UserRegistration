7d431b3af91cf951be76ce87bdf3d5effff52c12
ronmeher <ronmeher@gmail.com>
2022-05-27 10:40:29 +0530

    First Name added!

--- Program.cs
+++ Program.cs
@@ -11,7 +11,15 @@ namespace UserRegistration
     internal class Program
     {
 
+        public static string FIRSTNAME = "^[A-Z][a-z]*$";
         
+        public bool validateFirstName()
+        {
+            Console.WriteLine("Enter your First Name");
+            string firstname = Console.ReadLine();
+            return Regex.IsMatch(firstname, FIRSTNAME);
+        }
+
         
 
 
@@ -23,7 +31,9 @@ namespace UserRegistration
         {
             Console.WriteLine("USER REGISTRATION PROGRAM!\n");
 
-            
+            Program p = new Program();
+            Console.WriteLine(p.validateFirstName());
+        
         }
     }
 }
