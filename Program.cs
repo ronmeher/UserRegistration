c765a5e695debe50c32aeace4cf5b341ae3b076c
ronmeher <ronmeher@gmail.com>
2022-05-27 10:48:05 +0530

    Last Name added!

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@ namespace UserRegistration
     {
 
         public static string FIRSTNAME = "^[A-Z][a-z]*$";
-        
+        public static string LASTNAME = "^[A-Z][a-z]*$";
+
+
         public bool validateFirstName()
         {
             Console.WriteLine("Enter your First Name");
@@ -20,7 +22,12 @@ namespace UserRegistration
             return Regex.IsMatch(firstname, FIRSTNAME);
         }
 
-        
+        public bool validateLastName()
+        {
+            Console.WriteLine("Enter your Last Name");
+            string lastname = Console.ReadLine();
+            return Regex.IsMatch(lastname, LASTNAME);
+        }
 
 
 
@@ -33,7 +40,8 @@ namespace UserRegistration
 
             Program p = new Program();
             Console.WriteLine(p.validateFirstName());
-        
+            Console.WriteLine(p.validateLastName());
+
         }
     }
 }
